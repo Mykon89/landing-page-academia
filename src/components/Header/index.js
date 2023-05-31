0bde130678d2adea418c27ae0779c360179092db
Mykon <helvis89@gmail.com>
2023-05-31 17:57:22 -0300

    media desconfigurada

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,38 +6,42 @@ import Logo from '../../assets/logo.svg';
 
 export default function Header() {
   return (
-    <nav className="Navbar">
-      <div className="navbar-logo">
-        <Link
-          to="home"
-          activeClass="active"
-          spy={true}
-          smooth={true}
-          offset={-195}
-          duration={1000}
-        >
-          <img src={Logo} alt="logo" />
-        </Link>
-      </div>
-      <div>
-        {MenuItems.map((item, index) => {
-          return (
-            <Link
-              key={index}
-              className="navbar-menu"
-              activeClass="active"
-              to={item.id}
-              spy={true}
-              smooth={true}
-              offset={-180}
-              duration={1000}
-            >
-              <p className={item.cname}>{item.text}</p>
-            </Link>
-          );
-        })}
-      </div>
-      <button className="navbar-button">Acessar</button>
-    </nav>
+    <section className="Header">
+      <nav className="Navbar">
+        <div className="navbar-logo">
+          <Link
+            to="home"
+            activeClass="active"
+            spy={true}
+            smooth={true}
+            offset={-195}
+            duration={1000}
+          >
+            <img src={Logo} alt="logo" />
+          </Link>
+        </div>
+        <div>
+          {MenuItems.map((item, index) => {
+            return (
+              <Link
+                key={index}
+                className="navbar-menu"
+                activeClass="active"
+                to={item.id}
+                spy={true}
+                smooth={true}
+                offset={-180}
+                duration={1000}
+              >
+                <p className={item.cname}>{item.text}</p>
+              </Link>
+            );
+          })}
+        </div>
+        <div>
+          <button className="navbar-button">Acessar</button>
+        </div>
+      </nav>
+    </section>
   );
 }
